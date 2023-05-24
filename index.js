a50ea851a0ee2b29374db64d611bec5a63e9d77c
rauldevilla <raul.devilla@gmail.com>
2023-05-23 19:12:47 -0500

    Implements process router

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const { logger } = require("./util/logger.util");
 const { requestLogger } = require("./middleware/request-loggin.middleware");
 
+const processRoute = require('./routes/process.route');
+
 const port = 3000;
 
 const app = express();
 
 const addRoutes = () => {
-  app.get("/", (req, res) => {
-    res.send({ status: "OK" });
+  app.get('/', (req, res) => {
+    res.send({ status: 'OK' });
   });
+  app.use('/process', processRoute);
 };
 
 const addMiddlewares = () => {
--- routes/process.route.js
+++ routes/process.route.js
@@ -1,19 +1,23 @@
-const loggerFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} ${level}: ${message}`;
-});
+const express = require('express');
+const router = express.Router();
 
-const process = winston.createLogger({
-  level: "debug",
-  format: combine(
-    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), // Formato de fecha y hora deseado
-    loggerFormat
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "runfalcon-demo-application.log" }),
-  ],
-});
+const { logger } = require('../util/logger.util');
+const { getRandomName } = require('../util/data.util');
 
-module.exports = {
-  process,
+const buildReponse = () => {
+  var response = [];
+  for (index = 1; index <= 100; index++) {
+    response.push({
+      id: index,
+      name: getRandomName()
+    });
+  }
+  return response;
 };
+
+router.get('/', (req, res) => {
+    logger.debug('Process');
+    res.send(buildReponse());
+});
+
+module.exports = router;
